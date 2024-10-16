5e49b3ff8b2fa88fa7f4fb9749fba5221d1d6d41
amreldeeb2 <amr.eldeeb@hive-one.net>
2024-10-16 11:32:21 +0300

    upload commit

---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleEcommerce.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        
+        public string? Description { get; set; }
+        [Required]
+        public Decimal Price{ get; set; }
+        [Required]
+        public int StockQty{ get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using SimpleEcommerce.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// conn string
+// establish connection string 
+var connectionString = builder.Configuration.GetConnectionString("ProductDB");
+builder.Services.AddDbContext<ProductDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
